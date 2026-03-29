885dfa599b83e8ba50ffe8ab18f43f1e74af8b1f
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-03-29 13:37:52 +0530

    imprve Home Page

--- src/COMPONENTS/home/HomeHeroText.jsx
+++ src/COMPONENTS/home/HomeHeroText.jsx
@@ -1,23 +1,33 @@
-import React from 'react' 
+import React from 'react'
 import Video from './video'
 
 const homeHeroText = () => {
-  return (
-    <div className='font-[fontLight]  text-center'>
-        <div className='text-[9.5vw] flex justify-center items-center uppercase leading-[10vw]'>L'étincelle</div>
-        <div 
-        className='text-[9.5vw] flex justify-center items-center uppercase leading-0'>
+    return (
+        <div className='font-[fontLight]  text-center'>
+            <div className='text-[9.5vw] flex justify-center items-center uppercase leading-[10vw]'>L'étincelle</div>
+            <div
+                className='text-[9.5vw] flex justify-center items-center uppercase leading-0'>
                 qui
-            <div id="vdoDiv" className='h-[7.2vw] w-[16vw] p-[1px] mb-5 rounded-full overflow-hidden '>
-                <Video className='rounded-full' />
-            </div>
+                <div id="vdoDiv" className='h-[7.2vw] w-[16vw] p-[1px] mb-5 rounded-full overflow-hidden '>
+                    <Video className='rounded-full' />
+                </div>
                 génère
             </div>
-        <div className='text-[9.5vw] flex justify-center items-center uppercase leading-[10vw]'>la créativité</div>
+            <div className='text-[9.5vw] flex justify-center items-center uppercase leading-[10vw]'>la créativité</div>
+            <div className=' h-[15vw] flex justify-end items-center'>
+                <pre className=' text-sm font-[fontLight] text-left' >
+{`                  K72 est une agence qui pense
+chaque action pour nourrir la marque. Demain,
+dans 5 mois et dans 5 ans. On cherche la
+friction qui crée l’étincelle pour générer de
+l’émotion. Pour assurer une relation honnête,
+on est sans filtre, on dit ce qui doit être dit, on
+fait ce qui doit être fait.`}
+                </pre>
+            </div>
 
-
-    </div>
-  )
+        </div>
+    )
 }
 
 export default homeHeroText
