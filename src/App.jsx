e5be987a8b59c7b6a44062ff01fa08b08e02a1bf
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-04-01 20:52:46 +0530

    useLocation for apply animation when i change the path

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import React from 'react'
-import { Route, Routes , Link } from 'react-router-dom'
+
+import { Route, Routes, Link } from 'react-router-dom'
 import Home from './PAGES/Home'
 import Project from './PAGES/Project'
 import Agence from './PAGES/Agence'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
+
+
 
 const App = () => {
-  useGSAP(()=>{
-    // const tl = gsap.timeline()
-    gsap.from('.stair',{
-      height : 0,
-      duration:5,
-      stagger : {
-        amount:1
-      }
-    })
-  })
+
+
+
+
   return (
     <div >
-      <div className='h-screen w-full flex fixed top-0 z-10'>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-white'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-white'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-      </div>
+
       <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/projects' element={<Project/>} />
-          <Route path='/agence' element={<Agence/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Project />} />
+        <Route path='/agence' element={<Agence />} />
       </Routes>
     </div>
   )
---
+++ src/COMPONENTS/common/Stair.jsx
@@ -0,0 +1,55 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React, { useRef } from 'react'
+import { useLocation } from 'react-router-dom'
+
+const Stair = () => {
+    const parentDivRef = useRef(null)
+    const location = useLocation().pathname
+    useGSAP(() => {
+        const tl = gsap.timeline()
+
+        tl.to(parentDivRef.current, {
+            display: 'block'
+        })
+        tl.from('.stair', {
+            height: 0,
+            duration: 0.6,
+            stagger: {
+                amount: -0.25
+            }
+        })
+        tl.to('.stair', {
+            y: '100%',
+            duration: 0.5,
+            stagger: {
+                amount: -0.25
+            }
+        })
+        tl.to(parentDivRef.current, {
+            display: 'none'
+        })
+        tl.to('.stair', {
+            y: '0%',
+
+        })
+
+
+    },[location])
+    return (
+
+        <div>
+            <div ref={parentDivRef} className='h-screen w-full fixed top-0 z-10'>
+                <div className='h-full w-full flex '>
+                    <div className='stair h-full w-1/5 bg-black'></div>
+                    <div className='stair h-full w-1/5 bg-black'></div>
+                    <div className='stair h-full w-1/5 bg-black'></div>
+                    <div className='stair h-full w-1/5 bg-black'></div>
+                    <div className='stair h-full w-1/5 bg-black'></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Stair
