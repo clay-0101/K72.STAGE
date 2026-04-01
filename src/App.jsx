7f8f4a9026393a321be9fb5559d44b5bed9a30ac
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-04-01 18:52:58 +0530

    begin stair animation, also improve scrollTrigger image changing animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,29 @@ import { Route, Routes , Link } from 'react-router-dom'
 import Home from './PAGES/Home'
 import Project from './PAGES/Project'
 import Agence from './PAGES/Agence'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const App = () => {
+  useGSAP(()=>{
+    // const tl = gsap.timeline()
+    gsap.from('.stair',{
+      height : 0,
+      duration:5,
+      stagger : {
+        amount:1
+      }
+    })
+  })
   return (
     <div >
+      <div className='h-screen w-full flex fixed top-0 z-10'>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-white'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-white'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+      </div>
       <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/projects' element={<Project/>} />
