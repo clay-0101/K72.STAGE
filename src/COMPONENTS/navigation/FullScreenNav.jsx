6723b1308f55c48a24df438b1dd8cedc2d6673c6
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-04-07 21:30:02 +0530

    improve animation fullScreen Nav

--- src/COMPONENTS/navigation/FullScreenNav.jsx
+++ src/COMPONENTS/navigation/FullScreenNav.jsx
@@ -7,7 +7,7 @@ import MontrealClock from '../home/MontRealClock'
 
 const FullScreenNav = ({ navPresence }) => {
 
-    const stairsRef = useRef(null)
+    const rotateRef = useRef(null)
     const arrOfRows = [
         {
             title: 'PROJECT',
@@ -81,36 +81,33 @@ const FullScreenNav = ({ navPresence }) => {
             ease: 'linear'
         })
     }
-    useGSAP(() => {
-        const stairs = stairsRef.current.querySelectorAll('.stair')
-
-        const tl = gsap.timeline()
-
-        tl.to(stairs,{
-            height : '100%',
-           duration : 0.6,
-           stagger : {amount : -0.2}
+    useGSAP(()=>{
+        gsap.set('.rotateDiv',{rotationX:90})
+        gsap.to('.rotateDiv',{
+            rotationX : 0,
+            transformOrigin: "top center",
+            delay:2,
+            duration:0.5,
+            stagger : 0.05
         })
- 
-
-
     })
 
     return (
         <div
-            className='fullNavDiv pt-[1px] bg-black h-screen w-full overflow-hidden  absolute bottom-0 left-0 z-99'
-            style={{ display: `block` }}
+            className=' fullNavDiv pt-[1px] bg-black h-screen w-full overflow-hidden  absolute bottom-0 left-0 z-99'
+            style={{ display: `` }}
         >
-            <div className=' mt-[12vw]'>
+            <div className='rotateDiv mt-[12vw]'>
                 {arrOfRows.map((elem, idx) => {
                     return <div
                         key={idx}
+                        
                         onMouseEnter={MouseEnterHandler}
                         onMouseLeave={MouseLeaveHandler}
-                        className={`border-t-[0.5px] border-[#b3b3b3af] font-[fontBold] relative relative overflow-hidden ${idx === arrOfRows.length - 1 ? 'border-b-1' : ''}`}>
+                        className={`border-t-[0.5px]   border-[#b3b3b3af] font-[fontBold] relative relative overflow-hidden ${idx === arrOfRows.length - 1 ? 'border-b-1' : ''}`}>
 
 
-                        <h1 className='text-[8.5vw] text-center text-white leading-[8.5vw]'>{elem.title}</h1>
+                        <h1 className='rotateDiv text-[8.5vw] text-center  text-white leading-[8.5vw]'>{elem.title}</h1>
                         <div className='runningText   flex overflow-x-auto text-nowrap items-center bg-[#D3FD50] top-[0]  absolute h-[0%] uppercase'>
                             <h2 className='text-[8vw] leading-[0.8] pt-2.5 text-black'>{elem.movingText1}</h2>
                             <div className={`flex-shrink-0 overflow-hidden  mx-5 ${idx === 2 ? 'h-30 w-30' : 'h-25 w-[17vw] rounded-full'}`}>
@@ -138,16 +135,7 @@ const FullScreenNav = ({ navPresence }) => {
                 })}
             </div>
             <MontrealClock />
-            <div
-                ref={stairsRef}
-                className='h-screen w-full fixed top-0 z-100 flex overflow-hidden pointer-events-none'
-            >
-                <div className='stair h-[0%] w-1/5 bg-black'></div>
-                <div className='stair h-[0%] w-1/5 bg-white'></div>
-                <div className='stair h-[0%] w-1/5 bg-black'></div>
-                <div className='stair h-[0%] w-1/5 bg-white'></div>
-                <div className='stair h-[0%] w-1/5 bg-black'></div>
-            </div>
+
         </div>
     )
 }
