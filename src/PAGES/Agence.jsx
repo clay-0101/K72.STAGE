12fab62dabe3f405d258c5c565ef1ac496f2c667
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-04-13 21:26:57 +0530

    improve agence page

--- src/PAGES/Agence.jsx
+++ src/PAGES/Agence.jsx
@@ -5,7 +5,7 @@ import { useGSAP } from '@gsap/react'
 import { ScrollTrigger } from 'gsap/all'
 
 const Agence = () => {
-  
+
   gsap.registerPlugin(ScrollTrigger)
   const imageDivRef = useRef(null)
   const imageRef = useRef(null)
@@ -27,20 +27,20 @@ const Agence = () => {
     '/img8.jpg',
     '/img8.jpg',
   ]
- 
-  useGSAP(()=>{
-    gsap.to(imageDivRef.current,{
-      scrollTrigger:{
+
+  useGSAP(() => {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
         trigger: imageDivRef.current,
-        start:'top 27.8%',
-        end : 'top -100%',
-        scrub:true,
-        pin:true,
-        
-        onUpdate:(elem)=>{
+        start: 'top 27.8%',
+        end: 'top -100%',
+        scrub: true,
+        pin: true,
+
+        onUpdate: (elem) => {
           const imgIndex = Math.min(
             Math.floor(elem.progress * imageArray.length),
-            imageArray.length-1
+            imageArray.length - 1
           )
           imageRef.current.src = imageArray[imgIndex]
         }
@@ -53,7 +53,7 @@ const Agence = () => {
 
         <div ref={imageDivRef} className='h-[20vw] w-[15vw]  absolute top-[15vw] left-[30vw]'>
           <div className='h-full w-full rounded-3xl overflow-hidden'>
-            <img  ref={imageRef} className='h-full w-full object-cover' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="img1" />
+            <img ref={imageRef} className='h-full w-full object-cover' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="img1" />
           </div>
         </div>
 
@@ -69,7 +69,25 @@ const Agence = () => {
         </div>
       </div>
       <div className='section2 h-screen w-full'>
-
+        <div className='w-full h-[25vw] font-[fontBold] text-lg flex justify-center gap-[5vw] mt-[10vw] '>
+          <div className='w-[25%] h-full flex flex-col justify-between '>
+            <h2>Expertise</h2>
+            <h2>Nos projets_ naissent dans l’humilité, grandissent dans la curiosité et vivent grâce à la créativité sous toutes ses formes.</h2>
+          </div>
+          <div className='w-[25%] h-full flex flex-col justify-between '>
+            <ul>
+              <li>Stratégie</li>
+              <li>Publicité</li>
+              <li>Branding</li>
+              <li>Design</li>
+              <li>Contenu</li>
+            </ul>
+            <h2>Notre création_ bouillonne dans un environnement où le talent a le goût d’exploser. Où on se sent libre d’être la meilleure version de soi-même.</h2>
+          </div>
+          <div className='w-[25%] h-full flex flex-col justify-end '>
+            <h2>Notre culture_ c’est l’ouverture aux autres. Point. Tout l’équipage participe à bâtir une agence dont on est fiers. </h2>
+          </div>
+        </div>
       </div>
     </div>
   )
